853015111daf4abec4994d4bd2a36ce557aa6b45
adam <mudi.adamz@gmail.com>
2021-10-09 02:31:24 +0700

    better mobile layout

--- bid-frontend/src/TablePaginationDemo.js
+++ bid-frontend/src/TablePaginationDemo.js
@@ -23,11 +23,12 @@ export default function TablePaginationDemo(props) {
 
     return (
         <Grid container spacing={2} sx={{ py: 4 }}>
-            <Grid item xs={4}>
+            <Grid item xs={6} md={4}>
                 <SplitButton/>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={6} md={4}>
                 <TextField
+                    fullWidth
                     onChange={(event)=>{
                         const search = event.target.value;
                         setFilter({...filter, search});
@@ -38,7 +39,7 @@ export default function TablePaginationDemo(props) {
                     variant="filled"
                 />
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={12} md={4}>
                 <TablePagination
                     rowsPerPageOptions={[]}
                     component="div"
